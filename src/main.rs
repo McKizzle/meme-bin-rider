2ed63d65e20b7be911035e9ded9a48f54960d7b0
Clinton McKay <ziesunkownemail@clueless.duh>
2016-12-31 19:18:53 -0700

    Successfully generate a random string.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,32 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rand;
+
+use rand::distributions::{IndependentSample, Range};
+
+static SAMPLE_SPACE: &'static [u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/")]
+fn post_index() -> String {
+    gen_code()
+}
+
+/**
+ * Generates a random unique code. 
+ */
+fn gen_code() -> String {
+    let mut rand = rand::thread_rng();
+    let between = Range::new(0, SAMPLE_SPACE.len());
+    (0..8).map(|_| SAMPLE_SPACE[between.ind_sample(&mut rand)] as char).collect()
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, post_index]).launch();
 }
 
